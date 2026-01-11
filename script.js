a557fdf733fb8aa4b6a6e78799a0a877adb7f9e2
Shakyasubin <shakya.subin@gmail.com>
2026-01-11 22:09:55 +0545

    JS Operators and Object

--- script.js
+++ script.js
@@ -1,2 +1,124 @@
-console.log("Hello World!");
-alert("Hello World!");
+// console.log("Hello World!");
+// alert("Hello World!");
+
+// ==== Variable (char, num, $, _) ====
+
+// == var, let, const
+
+// var name = "Subin Shakya";
+
+// == var
+// var name; //creating a variable
+// name = "Subin Shakya"; //assigning value
+// console.log(name);
+
+// ==let
+// let add = "Khusibu";
+// console.log(add);
+
+// ==const
+// const age = "33";
+// console.log(age);
+
+// console.log(name, age, add);
+
+// ==== Arithmetic Operators ====
+// const a = 5;
+// const b = 6;
+// const c = b;
+// let d = 5;
+
+// ==  1. Addition ( + )
+// const result = a + b + c;
+
+// ==  2. Subtraction ( - )
+// const result = a - b;
+
+// ==  3. Multiplication ( * )
+// const result = a * b;
+
+// ==  4. Exponentiation (ES2016)  ( ** )
+// const result = a ** b;
+
+// ==  5. Division ( / )
+// const result = a / b;
+
+// ==  6. Modulus (Division Remainder) ( %)
+// const result = a % b;
+
+// ==  7. Increment ( ++ )
+// d++;
+// --d;
+
+// ==  8. Decrement ( -- )
+// d--;
+// --d;
+
+// console.log(++d);
+// console.log(--d);
+// console.log(d++);
+// console.log(d--);
+// console.log(d);
+// console.log(result);
+
+// ==== JS - Math Object ====
+
+// 1. == Math.PI
+// const ans = Math.PI;
+// Provides PI value
+
+// 2. == Math.round(value1.value2)
+// const ans = Math.round(4.5);
+// Provides round value i.e. if decimal is more than .5 then upper value, if lower then lower value
+
+// 3. == Math.pow(value1,value2)
+// const ans = Math.pow(8, 3);
+// Provides value by multplying the first value times the second value mentioned
+// 8*8*8
+
+// 4. == Math.sqrt(value)
+// const ans = Math.sqrt(64);
+// Provides value by finding square root of the given value
+// 8*8 = 64 = 8
+
+// 5. == Math.ceil(value1.value2)
+// const ans = Math.ceil(4.4);
+// Provides the upper value ignoring the decimal
+
+// 6. == Math.floor(value1, value2)
+// const ans = Math.floor(4.7);
+// Provides the lower value ignoring the decimal
+
+// 7. == Math.min(values)
+// const ans = Math.min(0, 150, 30, 20, -8, -200);
+// Provides value by finding the smallest value among the provided numbers
+
+// 8. == Math.max(values)
+// const ans = Math.max(0, 150, 30, 20, -8, -200);
+// Provides value by finding the largest value among the provided numbers
+
+// 9. == Math.random(value)
+// const ans = Math.random();
+// Provides random value between 0 and 1
+
+// console.log(ans);
+
+// ==== Comparision Operators ====
+const v1 = 5;
+const v2 = 10;
+
+// 1. == (checks only data)
+// const result = v1 == v2;
+
+// 2. === (checks data also data type)
+// const result = v1 === v2;
+
+// 3. >, >=
+// const result = v1 > v2;
+// const result = v1 >= v2;
+
+// 4. <, <=
+// const result = v1 < v2;
+// const result = v1 <= v2;
+
+console.log(result);
