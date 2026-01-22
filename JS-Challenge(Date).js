5da4c385b92268b9f40eb94ad50490a1c7093197
Shakyasubin <shakya.subin@gmail.com>
2026-01-22 23:43:52 +0545

    JavaScript Date

---
+++ JS-Challenge(Date).js
@@ -0,0 +1,5 @@
+// Date challange : Create a function that takes a food and the expire data and retuns one of the followings:
+
+// 1. if expired: AB is expired 4 days ago
+
+// 2. if not expired: you have 10 days to use this product.
---
+++ date-manipulation.js
@@ -0,0 +1,109 @@
+// ==== JS - Date Manipulation ====
+
+/// === Date() // sends data as string
+
+// const today = Date();
+
+// console.log(today);
+// console.log(typeof today);
+
+// =======================================
+
+/// === new Date() // sends data as object
+
+/// === new Date("yyyy-mm-dd") // useful to find exact info of the given day
+
+// const today = new Date();
+
+// console.log(today);
+// console.log(typeof today);
+
+// =======================================
+
+// === Date.now() // sends data in miliseconds
+
+// const today = Date.now();
+
+// console.log(today);
+
+// =======================================
+
+// === varName.getDate() // sends date
+
+// const today = new Date();
+// const data = today.getDate();
+
+// console.log(data);
+
+// =======================================
+
+// === varName.getDaye() // sends day
+
+// const today = new Date();
+// const data = today.getDay();
+
+// console.log(data);
+
+// =======================================
+
+// === varName.getFullYear() // sends year
+
+// const today = new Date();
+// const data = today.getFullYear();
+
+// console.log(data);
+
+// =======================================
+
+// === varName.getMonth() // sends month
+
+// const today = new Date();
+// const data = today.getMonth();
+
+// console.log(data);
+
+// =======================================
+
+// === varName.getMinutes() // sends minutes
+
+// const today = new Date();
+// const data = today.getMinutes();
+
+// console.log(data);
+
+// =======================================
+
+// === varName.toLocaleTimeString() // sends local time string i.e. hh:mm:ss am/pm
+
+// const today = new Date();
+// const data = today.toLocaleTimeString();
+
+// console.log(data);
+
+// =======================================
+
+// === varName.toLocaleDateString() // sends local date string i.e. mm/dd/yyyy
+
+// const today = new Date();
+// const data = today.toLocaleDateString();
+
+// console.log(data);
+
+// =======================================
+
+// === varName.toISOString() // sends data in ISO string useful for data base
+
+// const today = new Date();
+// const data = today.toISOString();
+
+// console.log(data);
+
+// =======================================
+
+// === varName.setDate(varName.getDate() condition) // sets a date
+
+// const today = new Date();
+
+// today.setDate(today.getDate() + 10);
+
+// console.log(today);
