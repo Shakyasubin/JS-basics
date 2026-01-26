dc2ca3a189afec6022f7319a292a676dc677aff8
Shakyasubin <shakya.subin@gmail.com>
2026-01-26 22:43:53 +0545

    JS Functions

--- JS-Challenge(Date).js
+++ JS-Challenge(Date).js
@@ -3,3 +3,32 @@
 // 1. if expired: AB is expired 4 days ago
 
 // 2. if not expired: you have 10 days to use this product.
+
+// Solution from Mentor
+
+// const foods = [
+//   { name: "Tuna", exp: "2026-1-20" },
+//   {
+//     name: "Milk",
+//     exp: "2026-1-30",
+//   },
+// ];
+
+// const milliSecPerDay = 24 * 60 * 60 * 1000;
+// const checkExp = ({ name, exp }) => {
+//   const today = Date.now();
+//   const expTs = new Date(exp).getTime();
+
+//   const diffDay = Math.floor((expTs - today) / milliSecPerDay);
+
+//   if (diffDay < 1) {
+//     return `${name} has expired ${Math.abs(diffDay)} days ago!`;
+//   } else {
+//     return `You have ${diffDay} to use the ${name}!`;
+//   }
+// };
+
+// foods.map((food) => {
+//   const result = checkExp(food);
+//   console.log(result);
+// });
---
+++ functions.js
@@ -0,0 +1,24 @@
+// ==== JS - Functions ====
+
+// === Classic Method ===
+
+// === function name() {}
+
+// function sayHi() {
+//   const str = "Hello there!, This is Subin!";
+//   console.log(str);
+// }
+//
+// sayHi();
+
+// =============================================
+
+// Sending data to function
+
+// function sayHi(name, add) {
+//   const str = "Hello there! This is " + name + "! I live in " + add + "!."; // concatination
+//   const str = `Hello there! This is ${name}!. I live in ${add}!.`; // template literal
+//   console.log(str);
+// }
+
+// sayHi("Spike", "Nayabazar");
